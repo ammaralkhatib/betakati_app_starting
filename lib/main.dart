861eb18ccdf33318905ace955423dda7fe56ccaa
Ammar Al Khatib <ammar.mngr@gmail.com>
2020-11-29 16:03:21 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[700],
+        body: Container(),
+      ),
+    ),
+  );
+}
